e0c05539531d54684c80b3741f854c5b4915e629
svyatoslav <svyatoslavlot@gmail.com>
2025-08-06 23:07:45 +0300

    Add some sections

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ window.addEventListener('scroll', () => {
   const section2 = document.getElementById('section2');
   const section3 = document.getElementById('section3');
   const section4 = document.getElementById('section4');
+
+  const projectPageSectionAboutContent = document.getElementById('scrollText');
+
   const img2 = document.querySelector('.img2');
 
   const maxScroll = vh;
@@ -20,6 +23,9 @@ window.addEventListener('scroll', () => {
 
   section2.style.transform = `scale(${0.9 + progress * 0.1})`;
   section2.style.opacity = `${progress}`;
+
+  projectPageSectionAboutContent.style.transform = `scale(${1 - progress * 0.2})`;
+  projectPageSectionAboutContent.style.opacity = `${progress}`;
 
   const section3Top = section3.offsetTop;
   const progress3 = Math.min(Math.max((scrollY - section3Top + vh) / vh, 0), 1);
